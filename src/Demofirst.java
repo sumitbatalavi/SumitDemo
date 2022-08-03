4758734a96fc9a0f01df4f191ffa9c2b0b4e8671
sowmya-anagandula <110163164+sowmya-anagandula@users.noreply.github.com>
2022-08-03 14:20:10 +0530

    03-08-2022
    i have chganged

--- src/Demofirst.java
+++ src/Demofirst.java
@@ -3,7 +3,7 @@ public class Demofirst {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hi Sowmya");
+		System.out.println("Hi prema");
 	}
 
 }
