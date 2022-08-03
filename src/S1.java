7383de39506b9b8a887531eb2f4befad7f9e5efa
jash-ikon <jashikon@gmail.com>
2022-08-04 00:07:56 +0530

    4/8/2022
    Another push by Jash

---
+++ src/S1.java
@@ -0,0 +1,17 @@
+
+public class S1{
+
+	public static void main(String[] args) {
+		
+				
+		S.poop();
+		System.out.println(S.color);
+		
+		
+		S.color = "bluess";
+		
+		System.out.println(S.color); 
+
+	}
+
+}
